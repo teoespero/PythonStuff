619a426c768ac832a8c7e94aa3eae3b296f0cab5
Teo Espero <38012835+teoespero@users.noreply.github.com>
2020-05-09 16:12:14 -0700

    Add files via upload

---
+++ list-manip-03.py
@@ -0,0 +1,30 @@
+# More Python list manipulation
+# Teo Espero
+# GIST I
+
+# BOF
+
+# initialize our vars
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# print the current list
+print('print the current list')
+print(cars)
+
+# sort the elements on the list
+print('sort the elements on the list')
+cars.sort()
+
+# print the sorted list
+print('print the sorted list')
+print(cars)
+
+# reverse sort the elements on the list
+print('reverse sort the elements on the list')
+cars.sort(reverse=True)
+
+# print the reverse sorted list
+print('print the reverse sorted list')
+print(cars)
+
+# EOF
