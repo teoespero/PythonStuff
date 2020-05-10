112dc5849f79ba235745e80738710368e43dbeb4
Teo Espero <38012835+teoespero@users.noreply.github.com>
2020-05-09 18:51:38 -0700

    Add files via upload

---
+++ Chapter-06/dictionaries-03.py
@@ -0,0 +1,18 @@
+# Still looping through a dictionary
+# Teo Espero
+# GIST I
+
+# BOF
+
+# define our dictionary
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+for name in favorite_language.keys():
+    print(name.title())
+
+# EOF
