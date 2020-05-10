ba7fbccfdb41075f70343e6bce5bcd3831941916
Teo Espero <38012835+teoespero@users.noreply.github.com>
2020-05-09 17:53:52 -0700

    Add files via upload

---
+++ Chapter-05/if-statements-02.py
@@ -0,0 +1,8 @@
+# checking whether a value is in the list
+# Teo Espero
+# GIST I
+
+# BOF
+
+
+# EOF
---
+++ Chapter-05/if-statments-01.py
@@ -0,0 +1,20 @@
+# If Statements in Python
+# Teo Espero
+# GIST I
+
+# BOF
+
+cars = [
+        'bmw',
+        'audi',
+        'subaru',
+        'toyota'
+]
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+# EOF
